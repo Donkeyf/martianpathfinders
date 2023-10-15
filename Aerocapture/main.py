4bd0b442ea5a5c8b354e148b14a3c0c09d9dbf21
DilonH <42641079+Donkeyf@users.noreply.github.com>
2023-10-16 03:11:05 +1100

    Add files via upload

--- Aerocapture/main.py
+++ Aerocapture/main.py
@@ -32,7 +32,7 @@ ma_a = 0
 T_a = np.sqrt((a_a**3)/(mu/(4*np.pi**2)))
 n_a = 2 * np.pi / T_a
 
-t_sec_a = np.arange(0, T_a, 1)
+t_sec_a = np.arange(0, T_a/2, 1)
 ta_a, ea, ma_vec = ot.compute_anomalies(t_sec_a, ma_a, e_a, n_a)
 
 ta_air = []
@@ -53,27 +53,36 @@ y0 = [x_a[0], y_a[0], z_a[0], dx[0], dy[0], dz[0]]
 print(y0)
 def drag(t, f):
     m = 31700
-    x = f[0]
-    y = f[1]
-    z = f[2]
-    vx = f[3]
-    vy = f[4]
-    vz = f[5]
+    # x = f[0]
+    # y = f[1]
+    # z = f[2]
+    # vx = f[3]
+    # vy = f[4]
+    # vz = f[5]
 
-    v = np.linalg.norm([vx, vy, vz])
-    r = np.linalg.norm([x, y, z])
+    # v = np.linalg.norm([vx, vy, vz])
+    # r = np.linalg.norm([x, y, z])
 
-    ax = -mu * x/(r**3) *  - (0.0587 * v**2)/m * (vx/v)
-    ay = -mu * y/(r**3) *  - (0.0587 * v**2)/m * (vy/v)
-    az = -mu * z/(r**3) *  - (0.0587 * v**2)/m * (vz/v)
-    return [vx, vy, vz, ax, ay, az]
+    # ax = -mu * x/(r**3) *  - (0.0587 * v**2)/m * (vx/v)
+    # ay = -mu * y/(r**3) *  - (0.0587 * v**2)/m * (vy/v)
+    # az = -mu * z/(r**3) *  - (0.0587 * v**2)/m * (vz/v)
+    # return [vx, vy, vz, ax, ay, az]
 
-#dragpath = sp.integrate.RK45(drag, 0, y0, 2223, rtol=1e-6, atol=1e-6, vectorized=True)
+    r_vec = f[:3] #position
+    r_dot = f[3:] #velocity
+
+    r = np.linalg.norm(r_vec)
+    v = np.linalg.norm(r_dot)
+
+    r_ddot = -mu/(r**3) * r_vec - (0.0587 * v * r_dot)/m
+
+    return np.concatenate((r_dot, r_ddot))
+
+dragpath = sp.integrate.RK45(drag, 0, y0, 2223, rtol=1e-6, atol=1e-6, vectorized=True)
 t_span = [0,2223] 
 t_val = np.arange(0, 2223, 1)
-dragpath = sp.integrate.solve_ivp(drag, t_span, y0, method='RK45', t_eval=t_val, dense_output=True, rtol=1e-6, atol=1e-6)
+dragpath = sp.integrate.solve_ivp(drag, t_span, y0, dense_output=True, rtol=1e-6, atol=1e-6)
 
-kms = dragpath.y
 #Plotting Mars
 r_mars = 3396.2e3
 u = np.linspace(0, 2 * np.pi, 100)
@@ -106,9 +115,12 @@ plt.show()
 plt.figure(4)
 ax = plt.axes(projection='3d')
 ax.plot3D(dragpath.y[0], dragpath.y[1], dragpath.y[2])
-#ax.plot_surface(x_mars, y_mars, z_mars)
+ax.plot_surface(x_mars, y_mars, z_mars, color='r')
 ax.set_title('')
 ax.set_xlabel("x (m)")
 ax.set_ylabel("y (m)")
 ax.set_zlabel("z (m)")
+ax.set_xlim(1e6, 3e6)
+ax.set_ylim(-3e6, -1e6)
+ax.set_aspect('equal')
 plt.show()
