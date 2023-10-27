e4162445ed42f4f9f7b8a7ec8ed127b7d3a925ff
oliviahayston <147309109+oliviahayston@users.noreply.github.com>
2023-10-27 19:26:54 +1100

    Add files via upload
    Main file

---
+++ Interplanetary DV/InterplanetaryDV.py
@@ -0,0 +1,19 @@
+from PorkchopPlot import PorkchopPlot
+import CombinedMinDV as combdv
+
+## Porkchop Plot for Earth to Starman (analysing launch dates over 2 years)
+earth_to_starman = PorkchopPlot('Earth',   'Starman',  0,1,12,2031, 0,1,10,2033,      120,                 15,24*30,                    60,               "pro",         120)
+earth_to_starman.get_plot()
+
+earth_to_starman.min_dv_toStarman()
+
+def total_dv():        
+    # Get total Delta-v of all segments (Earth to Starman to Mars to Earth)
+    es_dv = earth_to_starman.min_dv # 1. Find the minimum dv for Earth to Starman
+    se_dv = combdv.min_dvs_about_Mars() # 2-3. Find the minimum combined dv for Starman to Earth (via Mars)
+    esme_dv = float(es_dv) + float(se_dv)
+
+    print(f'TOTAL INTERPLANETARY DV RESULT: Optimised delta-v of {esme_dv} km/s for all interplanetary segments.')
+    return esme_dv
+
+total_dv()
